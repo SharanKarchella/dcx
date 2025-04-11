011e118acd748a11f716b89079826c23bfb6dbcd
SharanKarchella <sharankarchella@gmail.com>
2025-04-11 14:30:01 +0530

    Website-up

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import "./globals.css"; // Global styles
 import ThemeProvider from "./components/ThemeProvider"; // Theme Provider
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <ThemeProvider>
-  //   <App />
-  // </ThemeProvider>
-  <div>Under maintenance</div>
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
+  
 );
